e0e5c774566af0fdd5c7a1170068c465df033f6d
John Biesnecker <jbiesnecker@gmail.com>
2012-12-08 13:27:45 +0800

    updated readme

--- dice.hs
+++ dice.hs
@@ -1,5 +1,6 @@
 import System.Random
 import System.Environment
+import Control.Monad(replicateM_)
 
 handleArgs :: [String] -> [Int]
 handleArgs [x,y] = handleArgs [x,y,"1"]
@@ -12,7 +13,7 @@ tossDice g x y = take x $ randomRs (1, y) g
 main = do
   args <- getArgs
   let [x,y,z] = handleArgs args
-  sequence $ replicate z (do
+  replicateM_ z (do
     gen <- newStdGen
     let r = tossDice gen x y
     let s = sum r
